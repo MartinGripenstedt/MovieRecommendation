4c177fe3a2f51d550ba444e3159fb610e655a941
Martin Gripenstedt <martingripenstedt@hotmail.com>
2024-10-04 15:57:48 +0200

    Added generate new movie button, added add new movie form. Clean up code.

--- src/main/java/com/example/movierecommendation/Controller/MovieController.java
+++ src/main/java/com/example/movierecommendation/Controller/MovieController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -18,19 +17,21 @@ public class MovieController {
     public String index(Model model) {
         Movie randomMovie = movieService.getRandomMovie();
         model.addAttribute("movie", randomMovie);
+        model.addAttribute("newMovie", new Movie());
         return "index";
     }
 
-
-    @GetMapping("/add")
-    public String showAddForm(Model model) {
-        model.addAttribute("movie", new Movie());
-        return "add-movie";
+    @GetMapping("/random-movie")
+    public String getRandomMovie(Model model) {
+        Movie randomMovie = movieService.getRandomMovie();
+        model.addAttribute("movie", randomMovie);
+        model.addAttribute("newMovie", new Movie());
+        return "index";
     }
 
     @PostMapping("/add")
-    public String addMovie(@ModelAttribute Movie movie) {
-        movieService.addMovie(movie);
+    public String addMovie(Movie newMovie) {
+        movieService.addMovie(newMovie);
         return "redirect:/";
     }
 }
